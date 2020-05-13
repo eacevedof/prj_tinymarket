87339e724f4b1c6699a643be9495523269e4adbb
ioedumac <eacevedof@yahoo.es>
2020-05-13 15:11:44 +0200

    en public/index.php aplico useErrorHandler para ionos

--- backend_web/public/index.php
+++ backend_web/public/index.php
@@ -2,6 +2,36 @@
 //print_r($_POST);die;
 $time_start = microtime(true);
 
+$oErrHandler = set_error_handler("userErrorHandler");
+function userErrorHandler($errno,$errmsg,$filename,$linenum,$vars)
+{
+    $time=date("d M Y H:i:s");
+    // Get the error type from the error number
+    $errortype = array (1 => "Error",
+        2 => "Warning",
+        4 => "Parsing Error",
+        8 => "Notice",
+        16 => "Core Error",
+        32 => "Core Warning",
+        64 => "Compile Error",
+        128 => "Compile Warning",
+        256 => "User Error",
+        512 => "User Warning",
+        1024 => "User Notice");
+    $errlevel=$errortype[$errno];
+
+    //Write error to log file (CSV format)
+    $oErrCSV=fopen("errors.csv","a");
+    fputs($oErrCSV,"$time,$filename,$linenum,$errlevel,$errmsg\rn");
+    fclose($oErrCSV);
+
+    if($errno!=2 && $errno!=8)
+    {
+        //Terminate script if fatal error
+        die("A fatal error has occurred. Script execution has been aborted");
+    }
+}
+
 use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
