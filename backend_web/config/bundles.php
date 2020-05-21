8075463322b3d395f1f417afc0b1e3a717cdfcf5
ioedumac <eacevedof@yahoo.es>
2020-05-21 11:14:14 +0200

    nelmio-corsbundle instalado: composer require nelmio/cors-bundle

--- backend_web/config/bundles.php
+++ backend_web/config/bundles.php
@@ -13,4 +13,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
