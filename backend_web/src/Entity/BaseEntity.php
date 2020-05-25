c4ce94f8b87b8442606d854b01e4b5727d820887
ioedumac <eacevedof@yahoo.es>
2020-05-25 18:47:27 +0200

    sigue sin ir code_cache y no se porque

--- backend_web/src/Entity/BaseEntity.php
+++ backend_web/src/Entity/BaseEntity.php
@@ -120,12 +120,7 @@ class BaseEntity
      */
     protected $description = null;
 
-    /**
-     * @var string|null
-     * @Groups({all})
-     * @ORM\Column(name="code_cache", type="string", length=50, nullable=true, options={"default"="NULL"})
-     */
-    protected $codeCache = null;
+
 /**
  * ==========================================================================================================
  * ==========================================================================================================
